97acca665d2be71f3177459497e5a579f9c4f0b1
chanakaherath <chanakaherath78@gmail.com>
2023-10-08 17:14:56 +0530

    saved10.8.2023

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ReservationComponent } from './reservation/reservation.component';
+import { IdentificationComponent } from './identification/identification.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',redirectTo:'home',pathMatch:'full'},
+  {path:'home',component:HomeComponent},
+  {path:'reservation',component:ReservationComponent},
+  {path:'identification',component:IdentificationComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ReservationComponent } from './reservation/reservation.component';
+import { IdentificationComponent } from './identification/identification.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ReservationComponent,
+    IdentificationComponent
   ],
   imports: [
     BrowserModule,

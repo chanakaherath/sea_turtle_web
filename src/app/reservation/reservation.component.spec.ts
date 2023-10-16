856e61b842da124accd17525130fac8f4a28410e
chanakaherath <chanakaherath78@gmail.com>
2023-10-16 23:07:33 +0530

    10.16.2023

--- src/app/reservation/reservation.component.spec.ts
+++ src/app/reservation/reservation.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms'; // Import FormsModule if your component uses [(ngModel)]
+import { By } from '@angular/platform-browser'; // for querying the DOM
 
 import { ReservationComponent } from './reservation.component';
 
@@ -8,7 +10,8 @@ describe('ReservationComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ReservationComponent ]
+      declarations: [ ReservationComponent ],
+      imports: [ FormsModule ] // Add this if your component uses [(ngModel)]
     })
     .compileComponents();
 
@@ -20,4 +23,17 @@ describe('ReservationComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should not submit the form if any field is empty', () => {
+    const form = fixture.debugElement.query(By.css('.sign-up-form')).nativeElement;
+
+    // Here, we assume that the form submission is prevented by showing an alert message.
+    // We'll mock the window.alert function to track if it was called.
+    spyOn(window, 'alert').and.stub();
+
+    form.submit();
+
+    expect(window.alert).toHaveBeenCalledWith('Please fill all the fields');
+  });
 });
+

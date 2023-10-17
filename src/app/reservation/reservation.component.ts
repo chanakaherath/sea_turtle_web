28fc823e2022de62cd2b096ac0e81614fd7beccb
developer-arjunar <arjuna.r@coopinsu.com>
2023-10-17 15:56:40 +0530

    23/10/17_I

--- src/app/reservation/reservation.component.ts
+++ src/app/reservation/reservation.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ReservationService } from '../services/reservation.service';
 
 @Component({
   selector: 'app-reservation',
   templateUrl: './reservation.component.html',
   styleUrls: ['./reservation.component.css']
 })
-export class ReservationComponent {
+export class ReservationComponent implements OnInit {
+
+  firstName!: string;
+  lastName!: string;
+  email!: string;
+  contactNo!: string;
+
+  constructor(
+    private reservationService: ReservationService
+  ) {};
+
+  ngOnInit(): void {
+      
+  }
+
+  onSubmit() {
+    this.saveReservation();
+  }
+
+  saveReservation(): void {
+
+    let reservationModel = {
+      "firstName": this.firstName,
+      "lastName": this.lastName,
+      "email": this.email,
+      "contactNo": this.contactNo
+    }
+
+    this.reservationService.saveReservation(reservationModel).subscribe((data: any) => {
+      alert("Successfully Reserved...")
+    });
+  }
 
 }
